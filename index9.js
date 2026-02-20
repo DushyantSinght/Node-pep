449b78f1dd8d50604ef69a9825cb943c57998fae
Dushyant Singh Tanwar <139846713+DushyantSinght@users.noreply.github.com>
2026-02-20 15:28:55 +0530

    Refactor auth middleware and update route handling

--- index9.js
+++ index9.js
@@ -1,22 +1,24 @@
 import express from 'express';
 const app = express();
-const auth = (req,res,next)=>{
-    const {token} = req.params;
-    if(token === "1234"){
+
+const auth = (req, res, next) => {
+    const { token } = req.params;   
+
+    if (token === "1234") {
         next();
-    }
-    else{
+    } else {
         res.send("Not Authorised");
     }
-}
-app.get('/:token', (req,res) => {
-    app.use(auth);
-  res.send('Hello World!');
+};
+
+app.get('/:token', auth, (req, res) => {
+    res.send('Hello World!');
 });
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Public Route');
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
